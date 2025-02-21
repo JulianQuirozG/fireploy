edf7fee24c76a155ba08d21df1d5d1fa17fdd46f
JulianQuirozG <julianquga@ufps.edu.co>
2025-02-21 17:35:54 -0500

    feat: Se crearon los dto createy update, ademas de la implementacion del getall getbyid update create

--- src/modelos/materia/dto/create-materia.dto.ts
+++ src/modelos/materia/dto/create-materia.dto.ts
@@ -1,1 +1,30 @@
-export class CreateMateriaDto {}
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  IsIn,
+} from 'class-validator';
+
+export class CreateMateriaDto {
+  @IsDefined({ message: 'nombre es obligatorio' })
+  @IsNotEmpty({ message: 'nombre no puede estar vacío' })
+  @IsString({ message: 'nombre debe ser una cadena de texto' })
+  @MaxLength(50, { message: 'nombre no puede tener más de 50 caracteres' })
+  readonly nombre: string;
+
+  @IsDefined({ message: 'semestre es obligatorio' })
+  @IsNotEmpty({ message: 'semestre no puede estar vacío' })
+  @IsString({ message: 'semestre debe ser una cadena de texto' })
+  @MaxLength(5, { message: 'semestre no puede tener más de 5 caracteres' })
+  readonly semestre: string;
+
+  @IsDefined({ message: 'estado es obligatorio' })
+  @IsNotEmpty({ message: 'estado no puede estar vacío' })
+  @IsString({ message: 'estado debe ser una cadena de texto' })
+  @MaxLength(1, { message: 'estado debe tener solo 1 carácter' })
+  @IsIn(['A', 'I'], {
+    message: 'estado debe ser "A" para activo o "I" para inactivo',
+  })
+  readonly estado: string;
+}
--- src/modelos/materia/dto/update-materia.dto.ts
+++ src/modelos/materia/dto/update-materia.dto.ts
@@ -1,4 +1,30 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMateriaDto } from './create-materia.dto';
+import {
+  IsOptional,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  IsIn,
+} from 'class-validator';
 
-export class UpdateMateriaDto extends PartialType(CreateMateriaDto) {}
+export class UpdateMateriaDto {
+  @IsOptional()
+  @IsNotEmpty({ message: 'nombre no puede estar vacío' })
+  @IsString({ message: 'nombre debe ser una cadena de texto' })
+  @MaxLength(50, { message: 'nombre no puede tener más de 50 caracteres' })
+  readonly nombre?: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'semestre no puede estar vacío' })
+  @IsString({ message: 'semestre debe ser una cadena de texto' })
+  @MaxLength(5, { message: 'semestre no puede tener más de 5 caracteres' })
+  readonly semestre?: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'estado no puede estar vacío' })
+  @IsString({ message: 'estado debe ser una cadena de texto' })
+  @MaxLength(1, { message: 'estado debe tener solo 1 carácter' })
+  @IsIn(['A', 'I'], {
+    message: 'estado debe ser "A" para activo o "I" para inactivo',
+  })
+  readonly estado?: string;
+}
