20b8894a7521ec0a6ddc6cf3a9f26e91bd96b5d7
JulianQuirozG <julianquga@ufps.edu.co>
2025-02-22 12:07:04 -0500

    fix: create curso

--- src/modelos/curso/curso.module.ts
+++ src/modelos/curso/curso.module.ts
@@ -3,9 +3,16 @@ import { CursoService } from './curso.service';
 import { CursoController } from './curso.controller';
 import { Curso } from './entities/curso.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MateriaModule } from '../materia/materia.module';
+import { UsuarioModule } from '../usuario/usuario.module';
+import { Usuario } from '../usuario/entities/usuario.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Curso])],
+  imports: [
+    TypeOrmModule.forFeature([Curso, Usuario]),
+    MateriaModule,
+    UsuarioModule,
+  ],
   controllers: [CursoController],
   providers: [CursoService],
 })
