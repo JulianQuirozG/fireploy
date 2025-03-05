7590961364bf5c93d67d9ac10a4d7bd76889b0f7
JulianQuirozG <julianquga@ufps.edu.co>
2025-03-05 15:53:06 -0500

    Fix: Funcionalidad linux para puertos

--- src/services/system.service.ts
+++ src/services/system.service.ts
@@ -4,40 +4,34 @@ import { exec } from 'child_process';
 @Injectable()
 export class SystemService {
   private total_ports: number = 65535;
+
   constructor() {}
 
   async getAvailablePorts(): Promise<number[]> {
     return new Promise((resolve, reject) => {
       try {
-        exec('netstat -ano | findstr LISTENING', (error, stdout, stderr) => {
+        const command = `bash -c "comm -23 <(seq 0 65535) <(ss -tuln | awk '{print \\$4}' | awk -F':' '{print \\$NF}' | grep -E '^[0-9]+$' | sort -n | uniq)"`;
+
+        exec(command, (error, stdout, stderr) => {
           if (error || stderr) {
             // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors, @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
             reject(`Error ejecutando el comando: ${error || stderr}`);
             return;
           }
 
-          // Extraer los puertos en uso
-          const usedPorts = new Set<number>();
-          stdout.split('\n').forEach((line) => {
-            const match = line.match(/:(\d+)\s+/); // Buscar el número del puerto
-            if (match) {
-              usedPorts.add(parseInt(match[1], 10));
-            }
-          });
-
-          // Generar lista de puertos libres
-          const availablePorts: number[] = [];
-          for (let port = 1024; port <= this.total_ports; port++) {
-            // Evitar los puertos bajos (0-1023)
-            if (!usedPorts.has(port)) {
-              availablePorts.push(port);
-            }
-          }
+          // Convertir la salida en una lista de números
+          const availablePorts = stdout
+            .split('\n')
+            .map((line) => line.trim())
+            .filter((line) => line !== '')
+            .map(Number)
+            .filter((port) => port > 1023); // Excluir los puertos reservados (0-1023)
 
           resolve(availablePorts);
         });
       } catch (err) {
-        console.log(err);
+        // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+        reject(`Error general: ${err}`);
       }
     });
   }
