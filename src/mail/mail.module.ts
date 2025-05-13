f6886b1f3f7c50d27773b5f3aa307a3d05b6b9ec
Julian <iscalajul@gmail.com>
2025-05-12 23:20:12 -0500

    Fix: Arreglado los errores de audit

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,22 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        service: 'gmail',
-        auth: {
-          user: 'fireployvm@gmail.com',
-          pass: 'vhwb ulny tdcq bxtw',
-        },
-      },
-      defaults: {
-        from: '"Soporte Fireploy" <fireployvm@gmail.com>',
-      },
-    }),
-  ],
   providers: [MailService],
   exports: [MailService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,20 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  private transporter: nodemailer.Transporter
 
-  async enviarCorreo(
-    destinatario: string,
-    asunto: string,
-    mensajeHtml: string,
-  ) {
-    await this.mailerService.sendMail({
-      to: destinatario,
-      subject: asunto,
-      html: mensajeHtml,
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'fireployvm@gmail.com',
+        pass: 'vhwb ulny tdcq bxtw', // App Password de Gmail
+      },
     });
+  }
+
+  async enviarCorreo(to: string, subject: string, html: string) {
+    try{
+      const info = await this.transporter.sendMail({
+      from: '"Soporte Fireploy" <fireployvm@gmail.com>',
+      to,
+      subject,
+      html,
+    });
+
     return { message: 'Correo enviado con éxito' };
+    } catch (e){
+    throw new BadRequestException(`Error al enviar correo: ${e.message}`);
+    }
+    
   }
 }
